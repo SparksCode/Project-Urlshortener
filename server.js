6232ac656ea74b2de0e56b4fd691a89bfe685b65
Zachary Sparks <>
2021-04-28 14:56:01 +0000

    Established MongoDB connectivity.

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+
+// Connect to Mongo DB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Check Connection
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// Create URL Schema
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: String
+})
+const URL = mongoose.model("URL", urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +38,10 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// Create New URL
+
+// Get original url
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
